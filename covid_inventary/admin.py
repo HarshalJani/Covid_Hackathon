0d72156b136770c732e1687015340ee6c53b938c
varun pande <centrix94@gmail.com>
2020-06-14 03:29:43 -0500

    basic models(as per excel sheet) and backend created.

--- covid_inventary/admin.py
+++ covid_inventary/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import HospDeptInventory
+from .models import OrderInfo
+from .models import SellerInfo
+from .models import SellerItems
+
+admin.site.register(HospDeptInventory)
+admin.site.register(OrderInfo)
+admin.site.register(SellerInfo)
+admin.site.register(SellerItems)
--- inventory_hero/settings.py
+++ inventory_hero/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'covid_inventary',
+    'covid_inventary.apps.CovidInventaryConfig',
     'bootstrap3',
 ]
 
