60219b911dfc2ff201bc2bb82208b27b729b52df
varun pande <centrix94@gmail.com>
2020-06-15 00:43:53 -0500

    further update on views

--- covid_inventary/views.py
+++ covid_inventary/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from django.http import QueryDict
 
 # Create your views here.
 def index(request):
     return render(request,'covid_inventary/index.html')
+
+def dashboard(request):
+    return render(request,'covid_inventary/dashboard.html')
+
+def login(request):
+    return render(request,'covid_inventary/login.html')
+
+def inventory(request):
+    if request.method == 'POST':
+        if request.POST.__contains__('isVendor'):
+            return render(request,'covid_inventary/storage.html')
+        else:
+            return render(request,'covid_inventary/inventory.html')
